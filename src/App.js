fd9c847d61750323e1d01bdd46c2ae9f5bcb010b
ahsan2025 <ahsanakber7869@gmail.com>
2025-07-01 19:11:18 +0500

    Fix routing: wrap Routes with BrowserRouter and add basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import Index from './pages/Index';
-
 import Games1 from './pages/Games1';
 import Games2 from './pages/Games2';
 
@@ -11,21 +11,21 @@ import ProgressBar from './Components/ProgressBar';
 
 const App = () => {
   return (
-    <>
-     <ProgressBar/>
+    <BrowserRouter basename="/morragames">
+      <ProgressBar />
       <Routes>
-    
         <Route path='/' element={<Index />} />
         {/* <Route path='/archive' element={<Archive />} /> */}
-        {/* <Route path='/partner' element={<Partner />} />
-        <Route path='/news1' element={<News1 />} />
-        <Route path='/news2' element={<News2 />} /> */}
+        {/* <Route path='/partner' element={<Partner />} /> */}
+        {/* <Route path='/news1' element={<News1 />} /> */}
+        {/* <Route path='/news2' element={<News2 />} /> */}
         {/* <Route path='/contact' element={<Contact />} /> */}
         <Route path='/karnak-legacy' element={<Games1 />} />
         <Route path='/mythya' element={<Games2 />} />
       </Routes>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
