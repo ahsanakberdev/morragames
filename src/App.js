e3debfe138e3d1caa7752101d5fbffb8c07b7566
ahsan2025 <ahsanakber7869@gmail.com>
2025-07-01 19:33:24 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
 
-import './App.css';
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Index from './pages/Index';
 
-import Index from './pages/Index';
-import Games1 from './pages/Games1';
-import Games2 from './pages/Games2';
+// import Games1 from './pages/Games1';
+// import Games2 from './pages/Games2';
 
-import ProgressBar from './Components/ProgressBar';
+// import ProgressBar from './Components/ProgressBar';
+
+// const App = () => {
+//   return (
+//     <>
+//      <ProgressBar/>
+//       <Routes>
+    
+//         <Route path='/' element={<Index />} />
+//         {/* <Route path='/archive' element={<Archive />} /> */}
+//         {/* <Route path='/partner' element={<Partner />} />
+//         <Route path='/news1' element={<News1 />} />
+//         <Route path='/news2' element={<News2 />} /> */}
+//         {/* <Route path='/contact' element={<Contact />} /> */}
+//         <Route path='/karnak-legacy' element={<Games1 />} />
+//         <Route path='/mythya' element={<Games2 />} />
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+// ...
 
 const App = () => {
   return (
-    <BrowserRouter basename="/morragames">
+    <HashRouter>
       <ProgressBar />
       <Routes>
         <Route path='/' element={<Index />} />
         <Route path='/karnak-legacy' element={<Games1 />} />
         <Route path='/mythya' element={<Games2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
-}
-
-export default App;
+};
