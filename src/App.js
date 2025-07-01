029e8d7d147861649129534b5fecd0039a9c812c
ahsan2025 <ahsanakber7869@gmail.com>
2025-07-01 20:00:27 +0500

    vc

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-// import './App.css';
+import './App.css';
 
-// import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import Index from './pages/Index';
-
-// import Games1 from './pages/Games1';
-// import Games2 from './pages/Games2';
-
-// import ProgressBar from './Components/ProgressBar';
-
-// const App = () => {
-//   return (
-//     <>
-//      <ProgressBar/>
-//       <Routes>
-    
-//         <Route path='/' element={<Index />} />
-//         {/* <Route path='/archive' element={<Archive />} /> */}
-//         {/* <Route path='/partner' element={<Partner />} />
-//         <Route path='/news1' element={<News1 />} />
-//         <Route path='/news2' element={<News2 />} /> */}
-//         {/* <Route path='/contact' element={<Contact />} /> */}
-//         <Route path='/karnak-legacy' element={<Games1 />} />
-//         <Route path='/mythya' element={<Games2 />} />
-//       </Routes>
-//     </>
-//   )
-// }
-
-// export default App
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Index from './pages/Index';
+
+import Games1 from './pages/Games1';
+import Games2 from './pages/Games2';
+
+import ProgressBar from './Components/ProgressBar';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello from App!</h1>
-    </div>
-  );
-};
+    <>
+     <ProgressBar/>
+      <Routes>
+    
+        <Route path='/' element={<Index />} />
+        {/* <Route path='/archive' element={<Archive />} /> */}
+        {/* <Route path='/partner' element={<Partner />} />
+        <Route path='/news1' element={<News1 />} />
+        <Route path='/news2' element={<News2 />} /> */}
+        {/* <Route path='/contact' element={<Contact />} /> */}
+        <Route path='/karnak-legacy' element={<Games1 />} />
+        <Route path='/mythya' element={<Games2 />} />
+      </Routes>
+    </>
+  )
+}
 
-export default App;
+export default App
